2f2ec88e3dc95095e6eb37b5116a337c6f486cbe
krystianrola <Krystian.rola@upthrust.eu>
2022-05-20 15:15:11 +0200

    articletagcontroller rework

--- back-end/voucherland-api/app/Http/Controllers/ArticleTagController.php
+++ back-end/voucherland-api/app/Http/Controllers/ArticleTagController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
+use App\Models\ArticleTag;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class ArticleTagController extends Controller
 {
-    //
+
+
+    public function store(Article $article, Tag $tag)
+    {
+        ArticleTag::create(
+
+        );
+    }
 }
